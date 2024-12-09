e49ab187eab1c89c76d2f0086bc9f195541e63f1
hachimitsue <honeymaeomela@gmail.com>
2024-12-09 22:00:29 +0800

    refactor: streamline main.js by reordering imports and removing unused Vuetify setup
    Co-Authored-By: Jetross <170296192+jetrossgalinato@users.noreply.github.com>
    Co-Authored-By: lesliemaepelletero <160746664+lesliemaepelletero@users.noreply.github.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
-// import './assets/main.css'
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
@@ -6,24 +9,10 @@ import 'vuetify/styles'
 
 import vuetify from './plugins/vuetify'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
 
 app.use(createPinia())
 app.use(router)
